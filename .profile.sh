a0fcaf6d20a6875e630826f4310f10b77f85a8dd
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2019-08-27 23:46:14 +0200

    Add profile configs

--- .profile.sh
+++ .profile.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 eval "`dircolors ~/.dircolors`"
+
+parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+export PS1="\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+
+export PAGER=vim
+export EDITOR=vim
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alFh --color=auto'
+alias ls='ls --color=auto'
+alias mc='. /usr/lib/mc/mc-wrapper.sh'
+alias dusort='du -a . | sort -n -r | head -n 10'
+alias emacsx='emacs'
+alias emacs='emacs -nw'
+source /usr/local/bin/virtualenvwrapper.sh
